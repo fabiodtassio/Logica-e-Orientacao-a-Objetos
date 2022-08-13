54504394f1be2c2a53b2d4ea9bfc333aefa306ec
Fábio de Tássio <107265942+fabiodtassio@users.noreply.github.com>
2022-08-13 11:06:02 -0300

    Atividade 02 do Módulo 02

---
+++ Modulo.02/atividade02.py
@@ -0,0 +1,32 @@
+# SOFTEX-RECIFE
+
+# Atividade 02 do Módulo 02
+
+# Desenvolva um código que utilize as seguintes características de um veículo:
+# - Quantidade de rodas;
+# - Peso bruto em quilogramas;
+# - Quantidade de pessoas no veículo.
+# Com essas informações, o programa mostrará qual é a melhor categoria de habilitação para o veículo informado a partir das condições:
+# A: Veículos com duas ou três rodas;
+# B: Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg;
+# C: Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg;
+# D: Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas;
+# E: Veículos com quatro rodas ou mais e com mais de 6000 kg
+
+# Ínicio
+qtd_rodas = int(input('Digite a quantidade de rodas:'))
+qtd_pessoas = int(input('Digite a quantidade de pessoas:'))
+Peso = int(input('Didite o peso do veículo:'))
+
+if qtd_rodas == 2 or qtd_rodas == 3:
+    print('Categoria A')
+elif qtd_rodas == 4 and qtd_pessoas <= 8 and Peso <= 3500:
+    print('Categoria B')
+elif qtd_rodas >= 4 and qtd_pessoas <= 8 and Peso >= 3500 and Peso <= 6000:
+    print('Categoria C')
+elif qtd_rodas >= 4 and qtd_pessoas > 8 and Peso <= 6000:
+    print('Categoria D')  
+elif qtd_rodas >= 4 and Peso > 6000:
+    print('Categoria E')
+else:
+    print('O veículo não se enquadra em nenhuma categoria!')    
