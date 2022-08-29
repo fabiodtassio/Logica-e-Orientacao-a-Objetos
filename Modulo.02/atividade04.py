b98dcfab360060953656571f8c7a70c57a4ac85f
Fábio de Tássio <107265942+fabiodtassio@users.noreply.github.com>
2022-08-29 20:33:50 -0300

    Executar funções de códigos

--- Modulo.02/atividade04.py
+++ Modulo.02/atividade04.py
@@ -1,7 +1,7 @@
 # SOFTEX-RECIFE
 # Aluno: Fábio de Tássio
 
-# Atividade 04 do Módulo 04 (Executar funções de códigos)
+# Atividade 04 do Módulo 02 (Executar funções de códigos)
 
 # Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da
 # operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
